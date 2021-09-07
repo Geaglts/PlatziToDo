e9519f18e2965a438dbfe270a375fce3833e7980
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-09-07 11:15:50 -0500

    Componente TODO counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
+// custom components
+import { TodoCounter } from "./TodoCounter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter />
+    </>
   );
 }
 
