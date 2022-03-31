9ec3e24a01dd0bd5f3dc2169441bfdb008709ce9
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-03-31 14:55:38 -0600

    comments

--- src/TodoHeader/index.js
+++ src/TodoHeader/index.js
@@ -3,6 +3,7 @@ import { cloneElement, Children } from "react";
 export function TodoHeader({ children, loading }) {
   return (
     <header>
+      {/* Use of Children and cloneElement */}
       {Children.toArray(children).map((child) =>
         cloneElement(child, { loading })
       )}
