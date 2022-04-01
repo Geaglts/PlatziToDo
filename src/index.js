13d2f605831bb92c74595fed9f1da4702536935f
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-04-01 15:32:22 -0600

    Example of hoc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Card = (props) => {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>{props.name}</p>
+    </div>
+  );
+};
+
+function withName(name = "") {
+  return function (WrappedComponent) {
+    return function WrappedComponentWithName(props) {
+      return (
+        <>
+          <WrappedComponent name={name} {...props} />
+        </>
+      );
+    };
+  };
+}
+
+const CardWithName = withName("Miguel")(Card);
+
+ReactDOM.render(<CardWithName />, document.getElementById("root"));
