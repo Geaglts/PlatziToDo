b7628c91f9f8272a732092a744c15ba21770c360
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-04-01 15:36:21 -0600

    Notes about hoc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 // import App from "./App";
 
+/* 
+    Como comentario:
+    HOC: Hight Order Component
+    Todos los Componentes de Orden Mayor deben retornar al final
+    un componente comun y corriente
+
+    - Podemos solicitar algo de una API y al terminar podemos renderizar
+    un componente con los datos obtenidos.
+
+    - Tambien las podemos usar para validar rutas.
+
+    Hay muchos otros usos que se le pueden dar a las HOC pero 
+    ya queda a nuestra imaginacion donde podemos usarlas
+    y porque es una mejor alternativa depediendo del
+    contexto.
+*/
+
 const Card = (props) => {
   return (
     <div>
