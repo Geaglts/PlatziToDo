2721d6fc3902677b92276ce6aae6887079c617b9
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-04-03 10:16:51 -0500

    I put inside a modal the ChangeAlert component to block the background

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,15 +1,23 @@
 import "./ChangeAlert.css";
+import { Modal } from "../Modal";
 import { withStorageListener } from "./withStorageListener";
 
 function ChangeAlert({ show = false, toggleShow = () => {} }) {
   if (show) {
     return (
-      <div className="ChangeAlert_Container">
-        <p className="ChangeAlert_Message">Hay cambios en los datos</p>
-        <button className="ChangeAlert_RefreshButton" onClick={toggleShow}>
-          Recargar la pagina
-        </button>
-      </div>
+      <Modal blocked={true} state={show}>
+        <div className="ChangeAlert_Container">
+          <p className="ChangeAlert_Message">
+            Parece que ocurrieron cambios en otra pestaña del navegador
+          </p>
+          <p className="ChangeAlert_Message-small">
+            Te sugerimos que actualices tu información
+          </p>
+          <button className="ChangeAlert_RefreshButton" onClick={toggleShow}>
+            Actualizar
+          </button>
+        </div>
+      </Modal>
     );
   }
 
--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,14 +1,16 @@
 import "./Modal.css";
 import { createPortal } from "react-dom";
 
-function Modal({ children, state, toggleModal }) {
+function Modal({ children, state, toggleModal, blocked = false }) {
   if (!state) return null;
   return createPortal(
     <div className="Modal__Container">
       <div className="Modal__Content">
-        <button onClick={toggleModal} className="Modal__Button--close">
-          X
-        </button>
+        {!blocked && (
+          <button onClick={toggleModal} className="Modal__Button--close">
+            X
+          </button>
+        )}
         <div className="Modal__Content--div">{children}</div>
       </div>
     </div>,
